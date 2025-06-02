5de870e01ff4a3565e84c31c14c409279413cd45
Mehrab <rejbe323@gmail.com>
2025-06-02 14:41:48 +0600

    edit controller and program

--- Notes/Controllers/NoteController.cs
+++ Notes/Controllers/NoteController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Notes.Data;
 using Notes.Models;
 
@@ -6,6 +7,7 @@ namespace Notes.Controllers
 {
     public class NoteController : Controller
     {
+        
         private readonly ApplicationDbContext _context;
        
         public NoteController(ApplicationDbContext context)
